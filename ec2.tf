6d7d1d13ac6c6686a135e1ed144c66138e3068a7
Ronaldo Lemos <ronaldokz_33@hotmail.com>
2023-11-15 22:16:46 +0000

    region

--- ec2.tf
+++ ec2.tf
@@ -13,7 +13,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "sa-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_vpc" "main" {
--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 
 resource "aws_lambda_function" "lambda_terraform" {
   function_name = "lambda-terraform"
-  filename      = "lambda_function_payload.zip"
+  filename      = "Bpd.Order.Processing.Lambda.zip"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "Bpd.Order.Processing.Lambda::Bpd.Order.Processing.Lambda.Function::Handler"
 
