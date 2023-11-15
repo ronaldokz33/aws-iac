9a4d0d78ad5e19a73d55fb0c787377a29489cf4a
Ronaldo Lemos <ronaldokz_33@hotmail.com>
2023-11-15 22:11:08 +0000

    added filename

--- main.tf
+++ main.tf
@@ -18,6 +18,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 
 resource "aws_lambda_function" "lambda_terraform" {
   function_name = "lambda-terraform"
+  filename      = "lambda_function_payload.zip"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "Bpd.Order.Processing.Lambda::Bpd.Order.Processing.Lambda.Function::Handler"
 
